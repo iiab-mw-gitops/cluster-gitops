9d22e302c8d08b6a05eceb4d2e93a893a89dec2d
Anas Hijazi <anas_hijazi@jbs.com.jo>
2022-07-24 22:00:29 +0300

    Change Repo name

--- ace/environments/ci/pipelines/run-ace-is-pipeline.sh
+++ ace/environments/ci/pipelines/run-ace-is-pipeline.sh
@@ -7,9 +7,9 @@ tkn -n ci pipeline start \
   ace-integration-server \
   --param is-source-repo-url=git@github.com:${GIT_ORG}/${SOURCE_REPO}.git \
   --param is-infra-repo-url=git@github.com:${GIT_ORG}/ace-infra.git \
-  --param git-ops-repo-url=git@github.com:${GIT_ORG}/dr-cluster-gitops-apps.git \
-  --param gitops-apps-repo-full-name=${GIT_ORG}/dr-cluster-gitops-apps \
+  --param git-ops-repo-url=git@github.com:${GIT_ORG}/cluster-gitops.git \
+  --param gitops-apps-repo-full-name=${GIT_ORG}/cluster-gitops \
   --workspace name=shared-workspace,claimName=ace-integration-server \
   --workspace name=ace-infra-repo-secret,secret=ace-infra-at-github \
-  --workspace name=gitops-repo-secret,secret=dr-cluster-gitops-apps-at-github \
+  --workspace name=gitops-repo-secret,secret=cluster-gitops-at-github \
   --workspace name=ace-source-repo-secret,secret=${SOURCE_REPO}-at-github
